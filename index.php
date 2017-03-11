1b8cece85d2034a44e38784508192ab41c4850c8
Michael J. Hartwick <hartwick@hartwick.com>
2017-03-11 12:29:24 -0500

    * Update to generate Thunderbird autoconfig and Outlook auto config based on the RFC6186 DNS autodiscovery

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /* 
- * Copyright (C) 2016 Michael J. Hartwick <hartwick at hartwick.com>
+ * Copyright (C) 2016, 2017 Michael J. Hartwick <hartwick at hartwick.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,7 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-	include_once(dirname(__FILE__)."/includes/db.inc");
+	include_once(dirname(__FILE__)."/vendor/autoload.php");
+	include_once(dirname(__FILE__)."/includes/config.inc");
 	$requesturi = filter_input(INPUT_SERVER, "REQUEST_URI");
 	if(strncasecmp($requesturi, "/mail/config-v1.1.xml", 21) === 0) {
 		$agent = "Thunderbird";
@@ -39,191 +40,166 @@
 	} else {
 		$reply = new \DOMDocument('1.0', 'UTF-8');
 		$reply->formatOutput = \TRUE;
-		try {
-			$stmt = $pdo->prepare($query);
-			$stmt->execute(array($email));
-		} catch(\PDOException $ex) {
-			error_log($ex->getMessage());
-			exit(-3);
-		}
-		if($stmt->rowCount() > 0) {
-			$row = $stmt->fetch(\PDO::FETCH_NUM);
-			$addr = explode("@", $email);
-			$user = $addr[0];
-			$domain = $addr[1];
-			if($agent === "Thunderbird") {
-				$clientconfig = $reply->createElement("clientConfig");
-				$clientconfig->setAttribute("version", "1.1");
-				$emailprovider = $reply->createElement("emailProvider");
-				$emailprovider->setAttribute("id", $providerid);
-				$emailprovider->appendChild($reply->createElement("domain", $domain));
-				$emailprovider->appendChild($reply->createElement("displayName", $provider));
-				if($row[2] === "N" || $row[2] === '0') {
-					if(isset($servers['IMAPS'])) {
-						$imaps = $reply->createElement("incomingServer");
-						$imaps->setAttribute("type", "imap");
-						$imaps->appendChild($reply->createElement("hostname", $servers['IMAPS']['server']));
-						$imaps->appendChild($reply->createElement("port", $servers['IMAPS']['port']));
-						$imaps->appendChild($reply->createElement("socketType", "SSL"));
-						$imaps->appendChild($reply->createElement("authentication", "password-cleartext"));
-						$imaps->appendChild($reply->createElement("username", $email));
-						$emailprovider->appendChild($imaps);
-					}
-					if(isset($servers['IMAP'])) {
-						$imap = $reply->createElement("incomingServer");
-						$imap->setAttribute("type", "imap");
-						$imap->appendChild($reply->createElement("hostname", $servers['IMAP']['server']));
-						$imap->appendChild($reply->createElement("port", $servers['IMAP']['port']));
-						if($servers['IMAP']['starttls'] === \TRUE) {
-							$imap->appendChild($reply->createElement("socketType", "STARTTLS"));
-						} else {
-							$imap->appendChild($reply->createElement("socketType", "plain"));
-						}
-						$imap->appendChild($reply->createElement("authentication", "password-cleartext"));
-						$imap->appendChild($reply->createElement("username", $email));
-						$emailprovider->appendChild($imap);
-					}
-				}
-				if($row[1] === "N" || $row[1] === '0') {
-					if(isset($servers['POP3S'])) {
-						$pop3s = $reply->createElement("incomingServer");
-						$pop3s->setAttribute("type", "pop3");
-						$pop3s->appendChild($reply->createElement("hostname", $servers['POP3S']['server']));
-						$pop3s->appendChild($reply->createElement("port", $servers['POP3S']['port']));
-						$pop3s->appendChild($reply->createElement("socketType", "SSL"));
-						$pop3s->appendChild($reply->createElement("authentication", "password-cleartext"));
-						$pop3s->appendChild($reply->createElement("username", $email));
-						$emailprovider->appendChild($pop3s);
-					}
-					if(isset($servers['POP3'])) {
-						$pop3 = $reply->createElement("incomingServer");
-						$pop3->setAttribute("type", "pop3");
-						$pop3->appendChild($reply->createElement("hostname", $servers['POP3']['server']));
-						$pop3->appendChild($reply->createElement("port", $servers['POP3']['port']));
-						if($servers['POP3']['starttls'] === \TRUE) {
-							$pop3->appendChild($reply->createElement("socketType", "STARTTLS"));
-						} else {
-							$pop3->appendChild($reply->createElement("socketType", "plain"));
-						}
-						$pop3->appendChild($reply->createElement("authentication", "password-cleartext"));
-						$pop3->appendChild($reply->createElement("username", $email));
-						$emailprovider->appendChild($pop3);
-					}
-				}
-				if($row[3] === "Y" || $row[3] === '0') {
-					$smtp = $reply->createElement("outgoingServer");
-					$smtp->setAttribute("type", "smtp");
-					$smtp->appendChild($reply->createElement("hostname", $servers['SMTP']['server']));
-					$smtp->appendChild($reply->createElement("port", $servers['SMTP']['port']));
-					if($servers['SMTP']['starttls'] === \TRUE) {
-						$smtp->appendChild($reply->createElement("socketType", "STARTTLS"));
-					} else {
-						$smtp->appendChild($reply->createElement("socketType", "plain"));
-					}
-					$smtp->appendChild($reply->createElement("authentication", "password-cleartext"));
-					$smtp->appendChild($reply->createElement("username", $email));
-					$emailprovider->appendChild($smtp);
-				}
-				$clientconfig->appendChild($emailprovider);
-				$clientconfigupdate = $reply->createElement("clientConfigUpdate");
-				$servername = filter_input(INPUT_SERVER, "HTTP_HOST");
-				if(empty($servername)) {
-					$servername = filter_input(INPUT_SERVER, "SERVER_NAME");
-				}
-				$uri = filter_input(INPUT_SERVER, "REQUEST_URI");
-				$clientconfigupdate->setAttribute("url", "https://$servername$uri");
-				$clientconfig->appendChild($clientconfigupdate);
-				$reply->appendChild($clientconfig);
-			} elseif ($agent === "Outlook") {
-				$ad = $reply->createElementNS("http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006", "Autodiscover");
-				$response = $reply->createElementNS("http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a", "Response");
-				$user = $reply->createElement("User");
-				$user->appendChild($reply->createElement("DisplayName", $row[0]));
-				$response->appendChild($user);
-				$account = $reply->createElement("Account");
-				$account->appendChild($reply->createElement("AccountType", "email"));
-				$account->appendChild($reply->createElement("Action", "settings"));
-				$account->appendChild($reply->createElement("ServiceHome", $home));
-				if($row[1] === "N" || $row[1] === '0') {
-					if(isset($servers['POP3S'])) {
-						$protocol = $reply->createElement("Protocol");
-						$protocol->appendChild($reply->createElement("Type", "POP3"));
-						$protocol->appendChild($reply->createElement("Server", $servers['POP3']['server']));
-						$protocol->appendChild($reply->createElement("Port", $servers['POP3']['server']));
-						$protocol->appendChild($reply->createElement("LoginName", $email));
-						$protocol->appendChild($reply->createElement("DomainRequired", "on"));
-						$protocol->appendChild($reply->createElement("SPA", "off"));
-						$protocol->appendChild($reply->createElement("SSL", "on"));
-						$protocol->appendChild($reply->createElement("AuthRequired", "on"));
-						$protocol->appendChild($reply->createElement("TTL", "24"));
-						$account->appendChild($protocol);
-						unset($protocol);
-					}
-					if(isset($servers['POP3'])) {
-						$protocol = $reply->createElement("Protocol");
-						$protocol->appendChild($reply->createElement("Type", "POP3"));
-						$protocol->appendChild($reply->createElement("Server", $servers['POP3']['server']));
-						$protocol->appendChild($reply->createElement("Port", $servers['POP3']['server']));
-						$protocol->appendChild($reply->createElement("LoginName", $email));
-						$protocol->appendChild($reply->createElement("DomainRequired", "on"));
-						$protocol->appendChild($reply->createElement("SPA", "off"));
-						$protocol->appendChild($reply->createElement("SSL", "off"));
-						$protocol->appendChild($reply->createElement("AuthRequired", "on"));
-						$protocol->appendChild($reply->createElement("TTL", "24"));
-						$account->appendChild($protocol);
-						unset($protocol);
-					}
-				}
-				if($row[2] === "N" || $row[2] === '0') {
-					if(isset($servers['IMAPS'])) {
-						$protocol = $reply->createElement("Protocol");
-						$protocol->appendChild($reply->createElement("Type", "IMAP"));
-						$protocol->appendChild($reply->createElement("Server", $servers['IMAPS']['server']));
-						$protocol->appendChild($reply->createElement("Port", $servers['IMAPS']['server']));
-						$protocol->appendChild($reply->createElement("LoginName", $email));
-						$protocol->appendChild($reply->createElement("DomainRequired", "on"));
-						$protocol->appendChild($reply->createElement("SPA", "off"));
-						$protocol->appendChild($reply->createElement("SSL", "on"));
-						$protocol->appendChild($reply->createElement("AuthRequired", "on"));
-						$protocol->appendChild($reply->createElement("TTL", "24"));
-						$account->appendChild($protocol);
-						unset($protocol);
-					}
-					if(isset($servers['IMAP'])) {
-						$protocol = $reply->createElement("Protocol");
-						$protocol->appendChild($reply->createElement("Type", "IMAP"));
-						$protocol->appendChild($reply->createElement("Server", $servers['IMAP']['server']));
-						$protocol->appendChild($reply->createElement("Port", $servers['IMAP']['server']));
-						$protocol->appendChild($reply->createElement("LoginName", $email));
-						$protocol->appendChild($reply->createElement("DomainRequired", "on"));
-						$protocol->appendChild($reply->createElement("SPA", "off"));
-						$protocol->appendChild($reply->createElement("SSL", "off"));
-						$protocol->appendChild($reply->createElement("AuthRequired", "on"));
-						$protocol->appendChild($reply->createElement("TTL", "24"));
-						$account->appendChild($protocol);
-						unset($protocol);
-					}
-				}
-				if ($row[3] === "Y" || $row[3] === '0') {
-					$protocol = $reply->createElement("Protocol");
-					$protocol->appendChild($reply->createElement("Type", "SMTP"));
-					$protocol->appendChild($reply->createElement("Server", $servers['SMTP']['server']));
-					$protocol->appendChild($reply->createElement("Port", $servers['SMTP']['port']));
-					$protocol->appendChild($reply->createElement("LoginName", $email));
-					$protocol->appendChild($reply->createElement("DomainRequired", "on"));
-					$protocol->appendChild($reply->createElement("SPA", "off"));
-					$protocol->appendChild($reply->createElement("Encryption", "Auto"));
-					$protocol->appendChild($reply->createElement("AuthRequired", "on"));
-					$protocol->appendChild($reply->createElement("TTL", "24"));
-					$account->appendChild($protocol);
-					unset($protocol);
-				}
-				$response->appendChild($account);
-				$ad->appendChild($response);
-				$reply->appendChild($ad);
+		$addr = explode("@", $email);
+		$user = $addr[0];
+		$domain = $addr[1];
+		$subad = new \com\hartwick\autodiscover($domain);
+		$isad = new \com\hartwick\autodiscover($domain, "_imaps");
+		$iad = new \com\hartwick\autodiscover($domain, "_imap");
+		$psad = new \com\hartwick\autodiscover($domain, "_pop3s");
+		$pad = new \com\hartwick\autodiscover($domain, "_pop3");
+		$sub = $subad->CheckRR();
+		$is = $isad->CheckRR();
+		$i = $iad->CheckRR();
+		$ps = $psad->CheckRR();
+		$p = $pad->CheckRR();
+		if($agent === "Thunderbird") {
+			$clientconfig = $reply->createElement("clientConfig");
+			$clientconfig->setAttribute("version", "1.1");
+			$emailprovider = $reply->createElement("emailProvider");
+			$emailprovider->setAttribute("id", $providerid);
+			$emailprovider->appendChild($reply->createElement("domain", $domain));
+			$emailprovider->appendChild($reply->createElement("displayName", $provider));
+			if(!empty($is[0]) && $is[1] !== "0") {
+				$imaps = $reply->createElement("incomingServer");
+				$imaps->setAttribute("type", "imap");
+				$imaps->appendChild($reply->createElement("hostname", $is[0]));
+				$imaps->appendChild($reply->createElement("port", $is[1]));
+				$imaps->appendChild($reply->createElement("socketType", "SSL"));
+				$imaps->appendChild($reply->createElement("authentication", "password-cleartext"));
+				$imaps->appendChild($reply->createElement("username", $email));
+				$emailprovider->appendChild($imaps);
 			}
+			if(!empty($i[0]) && $i[1] !== "0") {
+				$imap = $reply->createElement("incomingServer");
+				$imap->setAttribute("type", "imap");
+				$imap->appendChild($reply->createElement("hostname", $i[0]));
+				$imap->appendChild($reply->createElement("port", $i[1]));
+				$imap->appendChild($reply->createElement("socketType", "plain"));
+				$imap->appendChild($reply->createElement("authentication", "password-cleartext"));
+				$imap->appendChild($reply->createElement("username", $email));
+				$emailprovider->appendChild($imap);
+			}
+			if(!empty($ps[0]) && $ps[1] !== "0") {
+				$pop3s = $reply->createElement("incomingServer");
+				$pop3s->setAttribute("type", "pop3");
+				$pop3s->appendChild($reply->createElement("hostname", $ps[0]));
+				$pop3s->appendChild($reply->createElement("port", $ps[1]));
+				$pop3s->appendChild($reply->createElement("socketType", "SSL"));
+				$pop3s->appendChild($reply->createElement("authentication", "password-cleartext"));
+				$pop3s->appendChild($reply->createElement("username", $email));
+				$emailprovider->appendChild($pop3s);
+			}
+			if(!empty($p[0]) && $p[1] !== "0") {
+				$pop3 = $reply->createElement("incomingServer");
+				$pop3->setAttribute("type", "pop3");
+				$pop3->appendChild($reply->createElement("hostname", $p[0]));
+				$pop3->appendChild($reply->createElement("port", $p[1]));
+				$pop3->appendChild($reply->createElement("socketType", "plain"));
+				$pop3->appendChild($reply->createElement("authentication", "password-cleartext"));
+				$pop3->appendChild($reply->createElement("username", $email));
+				$emailprovider->appendChild($pop3);
+			}
+			$smtp = $reply->createElement("outgoingServer");
+			$smtp->setAttribute("type", "smtp");
+			$smtp->appendChild($reply->createElement("hostname", $sub[0]));
+			$smtp->appendChild($reply->createElement("port", $sub[1]));
+			$smtp->appendChild($reply->createElement("socketType", "STARTTLS"));
+			$smtp->appendChild($reply->createElement("authentication", "password-cleartext"));
+			$smtp->appendChild($reply->createElement("username", $email));
+			$emailprovider->appendChild($smtp);
+			$clientconfig->appendChild($emailprovider);
+			$clientconfigupdate = $reply->createElement("clientConfigUpdate");
+			$servername = filter_input(INPUT_SERVER, "HTTP_HOST");
+			if(empty($servername)) {
+				$servername = filter_input(INPUT_SERVER, "SERVER_NAME");
+			}
+			$uri = filter_input(INPUT_SERVER, "REQUEST_URI");
+			$clientconfigupdate->setAttribute("url", "https://$servername$uri");
+			$clientconfig->appendChild($clientconfigupdate);
+			$reply->appendChild($clientconfig);
+		} elseif ($agent === "Outlook") {
+			$ad = $reply->createElementNS("http://schemas.microsoft.com/exchange/autodiscover/responseschema/2006", "Autodiscover");
+			$response = $reply->createElementNS("http://schemas.microsoft.com/exchange/autodiscover/outlook/responseschema/2006a", "Response");
+			$user = $reply->createElement("User");
+			$user->appendChild($reply->createElement("DisplayName", $email));
+			$response->appendChild($user);
+			$account = $reply->createElement("Account");
+			$account->appendChild($reply->createElement("AccountType", "email"));
+			$account->appendChild($reply->createElement("Action", "settings"));
+			$account->appendChild($reply->createElement("ServiceHome", $home));
+			if(!empty($ps[0]) && $ps[1] !== "0") {
+				$protocol = $reply->createElement("Protocol");
+				$protocol->appendChild($reply->createElement("Type", "POP3"));
+				$protocol->appendChild($reply->createElement("Server", $ps[0]));
+				$protocol->appendChild($reply->createElement("Port", $ps[1]));
+				$protocol->appendChild($reply->createElement("LoginName", $email));
+				$protocol->appendChild($reply->createElement("DomainRequired", "on"));
+				$protocol->appendChild($reply->createElement("SPA", "off"));
+				$protocol->appendChild($reply->createElement("SSL", "on"));
+				$protocol->appendChild($reply->createElement("AuthRequired", "on"));
+				$protocol->appendChild($reply->createElement("TTL", "24"));
+				$account->appendChild($protocol);
+				unset($protocol);
+			}
+			if(!empty($p[0]) && $p[1] !== "0") {
+				$protocol = $reply->createElement("Protocol");
+				$protocol->appendChild($reply->createElement("Type", "POP3"));
+				$protocol->appendChild($reply->createElement("Server", $p[0]));
+				$protocol->appendChild($reply->createElement("Port", $p[1]));
+				$protocol->appendChild($reply->createElement("LoginName", $email));
+				$protocol->appendChild($reply->createElement("DomainRequired", "on"));
+				$protocol->appendChild($reply->createElement("SPA", "off"));
+				$protocol->appendChild($reply->createElement("SSL", "off"));
+				$protocol->appendChild($reply->createElement("AuthRequired", "on"));
+				$protocol->appendChild($reply->createElement("TTL", "24"));
+				$account->appendChild($protocol);
+				unset($protocol);
+			}
+			if(!empty($is[0]) && $is[1] !== "0") {
+				$protocol = $reply->createElement("Protocol");
+				$protocol->appendChild($reply->createElement("Type", "IMAP"));
+				$protocol->appendChild($reply->createElement("Server", $is[0]));
+				$protocol->appendChild($reply->createElement("Port", $is[1]));
+				$protocol->appendChild($reply->createElement("LoginName", $email));
+				$protocol->appendChild($reply->createElement("DomainRequired", "on"));
+				$protocol->appendChild($reply->createElement("SPA", "off"));
+				$protocol->appendChild($reply->createElement("SSL", "on"));
+				$protocol->appendChild($reply->createElement("AuthRequired", "on"));
+				$protocol->appendChild($reply->createElement("TTL", "24"));
+				$account->appendChild($protocol);
+				unset($protocol);
+			}
+			if(!empty($i[0]) && $i[1] !== "0") {
+				$protocol = $reply->createElement("Protocol");
+				$protocol->appendChild($reply->createElement("Type", "IMAP"));
+				$protocol->appendChild($reply->createElement("Server", $i[0]));
+				$protocol->appendChild($reply->createElement("Port", $i[1]));
+				$protocol->appendChild($reply->createElement("LoginName", $email));
+				$protocol->appendChild($reply->createElement("DomainRequired", "on"));
+				$protocol->appendChild($reply->createElement("SPA", "off"));
+				$protocol->appendChild($reply->createElement("SSL", "off"));
+				$protocol->appendChild($reply->createElement("AuthRequired", "on"));
+				$protocol->appendChild($reply->createElement("TTL", "24"));
+				$account->appendChild($protocol);
+				unset($protocol);
+			}
+			$protocol = $reply->createElement("Protocol");
+			$protocol->appendChild($reply->createElement("Type", "SMTP"));
+			$protocol->appendChild($reply->createElement("Server", $sub[0]));
+			$protocol->appendChild($reply->createElement("Port", $sub[1]));
+			$protocol->appendChild($reply->createElement("LoginName", $email));
+			$protocol->appendChild($reply->createElement("DomainRequired", "on"));
+			$protocol->appendChild($reply->createElement("SPA", "off"));
+			$protocol->appendChild($reply->createElement("Encryption", "Auto"));
+			$protocol->appendChild($reply->createElement("AuthRequired", "on"));
+			$protocol->appendChild($reply->createElement("TTL", "24"));
+			$account->appendChild($protocol);
+			unset($protocol);
+			$response->appendChild($account);
+			$ad->appendChild($response);
+			$reply->appendChild($ad);
 		}
 	}
 	header("Content-type: application/xml");
 	echo $reply->saveXML();
-	error_log($reply->saveXML());
